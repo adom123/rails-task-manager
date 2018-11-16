a1371e381086272603fea583f2171ffd13c0c246
Emmanuel Asiama <wagonadom@gmail.com>
2018-11-16 18:58:13 +0200

    rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/show'
-  get 'tasks/create'
-  get 'tasks/update'
-  get 'tasks/delete'
-	resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
